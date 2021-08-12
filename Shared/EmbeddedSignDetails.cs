9459c2b536c3473c3f66ce81d1de69eeb3065fd3
Jayaprakash Kamaraj <jayaprakash.kamaraj@syncfusion.com>
2021-08-12 13:29:32 +0530

    blazor embed esign sample added

---
+++ Shared/EmbeddedSignDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBoldSignApp.Shared
+{
+    public class EmbeddedSignDetails
+    {
+        public string DocumentID { get; set; }
+        public string SignLink { get; set; }
+    }
+}
